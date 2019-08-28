60fc53c08a17d226b7ebd0008a73ccb64e9e8f3f
szk2s <suzukisjss@gmail.com>
2019-08-29 01:16:34 +0900

    refactor($root): organize folder structure

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,6 +2,6 @@
 #include "add1.cpp"
 int main(int argc, char** argv) {
   std::cout << "Hello World. I am Satoshi." << std::endl;
-  std::cout << "2 + 1 = " << add1(2) << std::endl;
+  std::cout << "4 + 1 = " << add1(4) << std::endl;
   return 0;
 }
