75bbcefe2802ea8a1f6b98cc8409ce0505100ae4
szk2s <suzukisjss@gmail.com>
2019-08-30 16:24:58 +0900

    feat($ui): call add1 function from UI

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
-#include "add1.cpp"
+#include "add1.h"
 int main(int argc, char** argv) {
-  std::cout << "Hello World. I am Satoshi." << std::endl;
-  std::cout << "4 + 1 = " << add1(4) << std::endl;
-  return 0;
+    add1(1);
+    return 0;
 }
